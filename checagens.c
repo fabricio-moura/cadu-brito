3bc088e00c9c5551d0da48f2533120aca72ca799
Fabrício Moura Jácome <fabriciomjacome@mailbox.org>
2019-11-11 17:49:58 +0000

    Makefile e pequenos updates

---
+++ checagens.c
@@ -0,0 +1,161 @@
+#include "checagens.h"
+
+int checar_primitiva(char *primitiva)
+{
+    if(strcmp(primitiva, "image") == 0)
+    {
+        return primitive_image;
+    }
+    else if(strcmp(primitiva, "color") == 0)
+    {
+        return primitive_color;
+    }
+    else if(strcmp(primitiva, "clear") == 0)
+    {
+        return primitive_clear;
+    }
+    else if(strcmp(primitiva, "rect") == 0)
+    {
+        return primitive_rect;
+    }
+    else if(strcmp(primitiva, "circle") == 0)
+    {
+        return primitive_circle;
+    }
+    else if(strcmp(primitiva, "polygon") == 0)
+    {
+        return primitive_polygon;
+    }
+    else if(strcmp(primitiva, "fill") == 0)
+    {
+        return primitive_fill;
+    }
+    else if(strcmp(primitiva, "save") == 0)
+    {
+        return primitive_save;
+    }
+    else if(strcmp(primitiva, "open") == 0)
+    {
+        return primitive_open;
+    }
+
+    return -1;
+}
+
+void checar_formato(char *formato)
+{
+    if(strcmp(formato, "P3") != 0)
+    {
+        printf("Formato de arquivo inválido!\n");
+        exit(1);
+    }
+}
+
+void checar_qualidade(int qualidade)
+{
+    if(qualidade != 255)
+    {
+        printf("Qualidade inválida. Abra um arquivo com qualidade 255.\n");
+        exit(1);
+    }
+}
+
+void checar_resolucao(imagem *ptr_desenho)
+{
+    if(ptr_desenho->X < 1 || ptr_desenho->Y < 1 ||
+        ptr_desenho->X > 4000 || ptr_desenho->Y > 4000)
+    {
+        printf("%d %d\n", ptr_desenho->X, ptr_desenho->Y);
+        printf("Resolução inválida! Insira uma resolução entre 1 e 4000.\n");
+        exit(-1);
+    }
+}
+
+void checar_mempixel(pixel **pixels, int counter)
+{
+    if(counter == -1)
+    {
+        if(pixels == NULL)
+        {
+            printf ("Erro! Memória Insuficiente\n");
+            exit(1);
+        }
+    }
+    else
+    {
+        if(pixels[counter] == NULL)
+        {
+            printf("Erro! Memória Insuficiente\n");
+            exit(1);
+        }
+    }
+}
+
+void checar_mempoligono(poligonal *poligono)
+{
+    if(poligono == NULL)
+    {
+        printf ("Erro! Memória Insuficiente\n");
+        exit(1);
+    }
+}
+
+void checar_fopen(FILE *arquivo)
+{
+    if(arquivo == NULL)
+    {
+        printf("Erro ao abrir arquivo!\n");
+        exit(1);
+    }
+}
+
+void checar_nome_input(char *ptr_input)
+{
+    printf("Insira o nome do arquivo txt ou 0 para default: ");
+    scanf("%s", ptr_input);
+    if(strcmp(ptr_input, "0") == 0)
+    {
+        strcpy(ptr_input, "input.txt");
+    }
+}
+
+void checar_coordenadas(int X, int Y, imagem *ptr_desenho, char *comando)
+{
+    if(X < 0 || Y < 0 || X > ptr_desenho->X-1 || Y > ptr_desenho->Y-1)
+    {
+        printf("Posição de coordenada do comando %s inválida.\n", comando);
+        exit(1);
+    }
+}
+
+int checar_proxpixel(int X, int Y, pixel ***ptr_pixels)
+{
+    if((*ptr_pixels)[X][Y].RGB.red != pincel_fill.RGB.red
+        || (*ptr_pixels)[X][Y].RGB.green != pincel_fill.RGB.green
+        || (*ptr_pixels)[X][Y].RGB.blue != pincel_fill.RGB.blue)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+void checar_cor(int red, int green, int blue, char *comando)
+{
+    if( red > 255 || red < 0
+        || green > 255 || green < 0
+        || blue > 255 || blue < 0)
+    {
+        printf("Cor inválida no comando %s.\n", comando);
+        exit(1);
+    }
+}
+
+void checar_pontos(poligonal **ptr_poligono)
+{
+    if((*ptr_poligono)->pontos < 3)
+    {
+        printf("Quantidade de pontos inválida.\n");
+        exit(1);
+    }
+}
---
+++ primitivas.h
@@ -0,0 +1,41 @@
+#ifndef PRIMITIVAS_H
+#define PRIMITIVAS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "tipos.h"
+#include "checagens.h"
+
+enum primitivas
+{
+    primitive_image,
+    primitive_color,
+    primitive_clear,
+    primitive_rect,
+    primitive_circle,
+    primitive_polygon,
+    primitive_fill,
+    primitive_save,
+    primitive_open
+};
+
+//void line_circle(int x_centro, int y_centro, int tamanho, pixel ***ptr_pixels);
+void circle_line(int x, int y, int decisao, pixel ***ptr_pixels);
+void fill_spread(unsigned short X, unsigned short Y, unsigned short imagem_X, unsigned short imagem_Y, pixel ***ptr_pixels);
+void paint_pixels(int eixo_x, int eixo_y, pixel ***ptr_pixels);
+void line(int x_final, int y_final, int x_inicial, int y_inicial, pixel ***ptr_pixels);
+void line_dx(int x_final, int y_final, int x_inicial, int y_inicial, int contador, pixel ***ptr_pixels);
+void line_dy(int x_final, int y_final, int x_inicial, int y_inicial, int contador, pixel ***ptr_pixels);
+void line_straight(int x_final, int y_final, int x_inicial, int y_inicial, pixel ***ptr_pixels);
+void image(FILE *arquivo, imagem *ptr_desenho, pixel ***ptr_pixels);
+void color(FILE *arquivo);
+void clear(FILE *arquivo, imagem *ptr_desenho, pixel ***ptr_pixels);
+void rect(FILE *arquivo, imagem *ptr_desenho, pixel ***ptr_pixels, poligonal **ptr_poligono);
+void circle(FILE *arquivo, imagem *ptr_desenho, pixel ***ptr_pixels);
+void polygon(FILE *arquivo, imagem *ptr_desenho, pixel ***ptr_pixels, poligonal **ptr_poligono);
+void fill(FILE *arquivo, imagem *ptr_desenho, pixel ***ptr_pixels);
+void save(FILE *arquivo_input, imagem *ptr_desenho, pixel ***ptr_pixels);
+void open(FILE *arquivo_input,imagem *ptr_desenho,  pixel ***ptr_pixels);
+
+#endif
