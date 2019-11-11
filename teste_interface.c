35eb1e9c98e55316865c47a1a7ccb0cd5ae4fc97
Fabrício Moura Jácome <fabriciomjacome@mailbox.org>
2019-11-11 02:55:10 +0000

    Botões com funções e novas janelas

--- teste_interface.c
+++ teste_interface.c
@@ -38,39 +38,148 @@
 // Make them global
 
 GtkWidget	*window;
+GtkWidget 	*window_save;
+GtkWidget 	*window_about;
 GtkWidget	*fixed1;
+GtkWidget 	*fixed2;
 GtkWidget	*button_clean;
 GtkWidget	*button_rect;
 GtkWidget	*button_circle;
 GtkWidget	*button_polygon;
 GtkWidget	*button_fill;
-GtkWidget	*combo1;
+GtkWidget	*basic_colors;
+GtkWidget 	*popover;
+GtkWidget 	*color_button;
+GtkWidget	*button_about;
+GtkWidget	*button_resolution;
+GtkWidget 	*button_save;
+GtkWidget 	*file_open;
+GtkWidget 	*save_name;
 GtkBuilder	*builder;
+
+void on_button_clean_clicked()
+{
+	printf("batata\n");
+}
+
+void on_button_new_activate()
+{
+	printf("novinho amigo\n");
+}
+
+void color_button_rgb(GtkColorButton *button)
+{
+	GdkRGBA *ptr_color, color;
+	int red, green, blue;
+	char cor[20];
+	ptr_color = &color;
+
+	gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(button), ptr_color);
+	sprintf(cor, "%s\n", gdk_rgba_to_string(ptr_color));
+	sscanf(cor, "%d,%d,%d", &red, &green, &blue);
+	printf("%d %d %d\n", red, green, blue);
+}
+
+void on_color_button_color_set()
+{
+	printf("vai vai\n");
+	color_button_rgb(GTK_COLOR_BUTTON(color_button));
+}
+
+void on_file_open_file_set()
+{
+
+}
+
+void on_button_about_activate()
+{
+	gtk_widget_show(window_about);
+}
+
+void on_button_resolution_activate()
+{
+
+}
+
+void on_button_save_activate()
+{
+	gtk_widget_show(window_save);
+}
+
+void save_name_activate_cb()
+{
+	printf("activate\n");
+	gtk_widget_hide(window_save);
+}
+
+void basic_colors_rgb(GtkEntry *entry)
+{
+	char cor[25];
+	int red, green, blue;
+	sprintf(cor, "%s", gtk_entry_get_text(entry));
+	printf("%s\n", cor);
+	sscanf(cor, "(%d, %d, %d)", &red, &green, &blue);
+	printf("%d %d %d\n", red, green, blue);
+}
+
+void on_basic_colors_changed()
+{
+	printf("colorido ido ido\n");
+	basic_colors_rgb(GTK_ENTRY(basic_colors));
+}
+
+
+// Iharob Al Asimi - Stack Overflow (how-do-i-intercept-a-gtk-window-close-button-click)
+gboolean on_widget_deleted(GtkWidget *widget, GdkEvent *event, gpointer data)
+{
+    gtk_widget_hide(widget);
+    return TRUE;
+}
 
 int main(int argc, char *argv[]) {
 
 	gtk_init(&argc, &argv); // init Gtk
 
-//---------------------------------------------------------------------
-// establish contact with xml code used to adjust widget settings
-//---------------------------------------------------------------------
-
 	builder = gtk_builder_new_from_file ("projeto.glade");
 
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+	window_save = GTK_WIDGET(gtk_builder_get_object(builder, "window_save"));
+	window_about = GTK_WIDGET(gtk_builder_get_object(builder, "window_about"));
 
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-        gtk_builder_connect_signals(builder, NULL);
+	// Iharob Al Asimi - Stack Overflow
+	g_signal_connect(G_OBJECT(window_save), "delete-event", G_CALLBACK(on_widget_deleted), NULL);
+	g_signal_connect(G_OBJECT(window_about), "delete-event", G_CALLBACK(on_widget_deleted), NULL);
+
+ 	gtk_builder_add_callback_symbol(builder, "on_button_clean_clicked", on_button_clean_clicked);
+	gtk_builder_add_callback_symbol(builder, "on_button_new_activate", on_button_new_activate);
+	gtk_builder_add_callback_symbol(builder, "on_file_open_file_set", on_file_open_file_set);
+	gtk_builder_add_callback_symbol(builder, "on_button_about_activate", on_button_about_activate);
+	gtk_builder_add_callback_symbol(builder, "on_button_resolution_activate", on_button_resolution_activate);
+	gtk_builder_add_callback_symbol(builder, "save_name_activate_cb", save_name_activate_cb);
+	gtk_builder_add_callback_symbol(builder, "on_button_save_activate", on_button_save_activate);
+	gtk_builder_add_callback_symbol(builder, "on_basic_colors_changed", on_basic_colors_changed);
+	gtk_builder_add_callback_symbol(builder, "on_color_button_color_set", on_color_button_color_set);
+
+
+    gtk_builder_connect_signals(builder, NULL);
 
 	fixed1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed1"));
+	fixed2 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed2"));
 	button_clean = GTK_WIDGET(gtk_builder_get_object(builder, "button_clean"));
 	button_rect = GTK_WIDGET(gtk_builder_get_object(builder, "button_rect"));
 	button_circle = GTK_WIDGET(gtk_builder_get_object(builder, "button_circle"));
 	button_polygon = GTK_WIDGET(gtk_builder_get_object(builder, "button_polygon"));
+	color_button = GTK_WIDGET(gtk_builder_get_object(builder, "color_button"));
 	button_fill = GTK_WIDGET(gtk_builder_get_object(builder, "button_fill"));
-	combo1 = GTK_WIDGET(gtk_builder_get_object(builder, "combo1"));
-
+	popover = GTK_WIDGET(gtk_builder_get_object(builder, "clear_color"));
+	basic_colors = GTK_WIDGET(gtk_builder_get_object(builder, "basic_colors"));
+	file_open = GTK_WIDGET(gtk_builder_get_object(builder, "file_open"));
+	button_about = GTK_WIDGET(gtk_builder_get_object(builder, "button_about"));
+	button_resolution = GTK_WIDGET(gtk_builder_get_object(builder, "button_resolution"));
+	button_save = GTK_WIDGET(gtk_builder_get_object(builder, "button_save"));
+	save_name = GTK_WIDGET(gtk_builder_get_object(builder, "save_name"));
 
 	gtk_widget_show(window);
 
